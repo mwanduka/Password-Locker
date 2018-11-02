8065f7127bbb1848f72e822209cc31a9f491ac3a
mwanduka <mwandukastephen20@gmail.com>
2018-11-02 22:16:08 +0300

    add classmethods

--- credentials.py
+++ credentials.py
@@ -1,24 +1,38 @@
-# class credential:
-#
-#         def __init__(self,account,username,password):
-#             self.account = account
-#             self.username = username
-#             self.password = password
-#
-#         def save_credential(self):
-#             '''
-#             save_credentials method saves credential object into the credential_list
-#             '''
-#             Credential.credential_list.append(self)
-#
-#         def delete_credential(self):
-#             '''
-#             delete_credential method deletes credential from of the credential_list
-#             '''
-#             Credential.credential_list.remove(self)
-#
-#         def password_generator(self):
-#             '''
-#             password_generator method creates credential to the credential_list
-#             '''
-#             Credential.credential_list. append(self)
+import string
+import random
+
+class Credentials:
+    '''
+    Class that generates new instances of credentials.
+    '''
+
+    credentials_list = []
+
+    def __init__(self, account, username, password):
+        '''
+        This method helps us define properties for our objects.
+        '''
+
+        self.account = account
+        self.username = username
+        self.password = password
+
+    def save_credentials(self):
+        '''
+        This methods save credential objects into the credentials_list.
+        '''
+
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        '''
+        This method deletes saved credentials from the credentials_list.
+        '''
+
+        Credentials.credentials_list.remove(self)
+
+    def password_generator(self, size=8, chars=string.ascii_letters + string.digits):
+        '''
+        This method generates a password by returning a string of random characters.
+        '''
+        return ''.join(random.choice(chars) for i in range(size))
