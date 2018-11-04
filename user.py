c1859afe867528161568b4a58d35ba775930b63c
mwanduka <mwandukastephen20@gmail.com>
2018-11-05 01:25:16 +0300

    add new file

--- user.py
+++ user.py
@@ -1,17 +1,17 @@
 class User:
-        # """
-        # Class that generates new instances of users
-        #             ``"""
-        # user_list = [] # Empty user list
-        #
-        def __init__(self,fname,lname,email,password):
+        """
+        Class that generates new instances of users
+        """
+        user_list = [] # Empty user list
+
+    def __init__(self, first_name, last_name, email_address, password):
 
           # docstring removed for simplicity
 
-            self.fname = fname
-            self.lname = lname
-            self.email = email
-            self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email_address = email_address
+        self.password = password
 
         def delete_user(self):
             '''
